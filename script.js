924946a9f9c40cd163ebc3d11a27fda2cde840cd
stephen etienne <sschneider725@gmail.com>
2024-08-01 09:09:57 -0400

    add remove button

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const author = document.getElementById("author");
 const title = document.getElementById("title");
 const pages = document.getElementById("pages");
 const removeBook = document.getElementById("removeBook");
+
 addBook.addEventListener("click", () => {
     modal.showModal();
 });
@@ -19,20 +20,37 @@ removeBook.addEventListener("click", deleteBook);
 
 function submitBook() {
     const newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "book");
     const pOne = document.createElement("p");
     const pTwo = document.createElement("p");
     const pThree = document.createElement("p");
+    const remove = document.createElement("button");
+
+    remove.setAttribute("id","remove");
+    remove.textContent = "remove book";
+    remove.addEventListener("click", removeEntry);
+
     pOne.innerHTML = author.value;
     pTwo.innerHTML = title.value;
     pThree.innerHTML = pages.value
     newDiv.appendChild(pOne);
     newDiv.appendChild(pTwo);
     newDiv.appendChild(pThree);
+    newDiv.appendChild(remove);
 
     books.appendChild(newDiv);
+    author.value = "";
+    title.value = "";
+    pages.value = "";
+
 }
 
 function deleteBook() {
-    const toDelete = books.lastChild;
+    const toDelete = books.lastElementChild;
+    toDelete.remove();
+}
+
+function removeEntry() {
+    const toDelete = this.parentElement;
     toDelete.remove();
 }
